950ea14eab2b587440a375c087605a1d6d591ea7
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2026-03-06 19:44:09 -0600

    ts coments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://binarybrains-club.github.io/",
+  site: "https://binarybrains-club.github.io",
 });
